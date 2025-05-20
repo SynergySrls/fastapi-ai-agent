4788a0a8336816f6d61e051479b48e5a4da80e1d
SynergySrls <info@synergysrls.com>
2025-05-20 16:20:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "Online", "message": "API attiva dal tuo Colab con Ngrok 🚀"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    return {"status": "Online", "message": "API attiva dal tuo GPT con Railway 🚀"}
